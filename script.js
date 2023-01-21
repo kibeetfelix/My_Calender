4380dd27768146ac6af0c29d8ad9bf935c146d9f
kibeetfelix <felixkibeet@gmail.com>
2023-01-21 19:06:19 +0300

    javascript

---
+++ script.js
@@ -0,0 +1,18 @@
+const monthInx = new Date().getMonth();
+
+
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
